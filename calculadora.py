49f351f58f6ae5428bf3013671e7bb70a269900b
Thiago Oliveira <thiago.oliveiraso06@gmail.com>
2026-07-15 22:30:54 -0300

    fiz a inclusão da soma na calculadora

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+
+
+while True:
+    
+    numero_1 = int(input('digite um numero?'))
+
+    operacao = input('Digite a operação +, -, *, /')
+
+    numero_2 =  int(input('Qual segundo Numero?'))
+
+    if operacao == '+':
+        resultado = numero_1 + numero_2
+        print(resultado)
+
+
+
