dadfd21d637f4e590f59593ca4fdf107173980d9
Thiago Oliveira <thiago.oliveiraso06@gmail.com>
2026-07-16 17:45:35 -0300

    Adicionei um sistema de saida

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,46 @@
  
+import os
 
 while True:
+    
+    sair = input('Digite "sair" ou aperte enter pra continuar:').lower()
+    if sair == 'sair':
+        print("Calculadora encerrada")
+        break
     try:
         numero_1 = int(input('digite um numero?'))
 
-        numero_2 =  int(input('Qual segundo Numero?'))
+        numero_2 =  int(input('Digite outro Numero?'))
     except ValueError:
         print('Numero inválido')
         continue
-    operacao = input('Digite a operação(+,-,x,/): ')
+    operacao = input('Digite a operação(+,-,x,/): ').lower()
 
     if operacao == '+':
+        
         resultado = numero_1 + numero_2
+
         print(resultado)
+        
 
     elif operacao == '-':
+        
         resultado = numero_1 - numero_2
         print(resultado)
+        
     elif operacao == 'x':
+      
         resultado = numero_1 * numero_2
         print(resultado)
     elif operacao == '/':
-        resultado = numero_1 / numero_2
-        print(resultado)
+        
+        if numero_2 ==  0:
+            print('Não é possivel dividir por zero')
+        else:
+            resultado = numero_1 / numero_2
+            print(resultado)
     
     else:
-        print("Digite apenas +, -, *, /")
+           print("Digite apenas +, -, x, /")
+
 
